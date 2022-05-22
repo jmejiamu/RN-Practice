cf26ebb7a276dda6f3920aae8477681673156dec
jmejiamu <jmejiamu@gmaillcom>
2022-05-21 20:59:03 -0700

    remove commented code

--- App.js
+++ App.js
@@ -54,29 +54,6 @@ const App = () => {
         onHandleDelete={onHandleDelete}
         setModalVisiable={setModalVisiable}
       />
-      {/* 
-      <Modal
-        animationType="slice"
-        visible={modalVisiable}
-        onRequestClose={() => null}
-      >
-        <View style={styles.modalContent}>
-          <View style={styles.modalTitleContainer}>
-            <Text style={styles.modalTitle}>Delete Item</Text>
-          </View>
-          <Text style={styles.modalText}>Are you sure?</Text>
-          <Text style={styles.modalMessage}>{itemSeleted?.value}</Text>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Button title="OK" onPress={() => onHandleDelete(itemSeleted)} />
-            <Button
-              title="Cancel"
-              onPress={() => setModalVisiable(!modalVisiable)}
-            />
-          </View>
-        </View>
-      </Modal> */}
     </View>
   );
 };
